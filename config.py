a6e7a8b1d3ba23938800fa5204c1a1e5e569d9e5
diane phan <dianephan@dianes-mbp.mynetworksettings.com>
2025-03-14 15:27:59 -0400

    🔧 configure all the relevant clients using env

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Configuration settings
+GOOGLE_MAPS_API = os.environ.get('GOOGLE_MAPS_API')
+LAUNCHDARKLY_SDK_KEY = os.environ.get('LAUNCHDARKLY_SDK_KEY')
+GOOGLE_MAPS_FLAG_KEY = "googlemaps"
+SECRET_KEY = 'dsfdgfdg;sdyyuyy' 
