766cf3c14cd57efc4eb69824888287695604ee1d
diane phan <dianephan@dianes-mbp.mynetworksettings.com>
2025-03-19 11:13:20 -0400

    🔧 add persisting postgres database

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ GOOGLE_MAPS_API = os.environ.get('GOOGLE_MAPS_API')
 LAUNCHDARKLY_SDK_KEY = os.environ.get('LAUNCHDARKLY_SDK_KEY')
 GOOGLE_MAPS_FLAG_KEY = "googlemaps"
 SECRET_KEY = 'dsfdgfdg;sdyyuyy' 
+DATABASE_URI = os.environ.get('DATABASE_URI') 
